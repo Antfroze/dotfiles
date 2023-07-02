c85d30b63b8db49a10c07da67890e0c97a125a25
Antfroze <realantfroze@gmail.com>
2023-07-01 20:12:28 -0500

    Updated showcase

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -46,30 +46,30 @@ local on_attach = function(client, bufnr)
 end
 
 protocol.CompletionItemKind = { "", -- Text
-    "",                          -- Method
-    "",                          -- Function
-    "",                          -- Constructor
-    "",                          -- Field
-    "",                          -- Variable
-    "",                          -- Class
-    "ﰮ",                          -- Interface
-    "",                          -- Module
-    "",                          -- Property
-    "",                          -- Unit
-    "",                          -- Value
-    "",                          -- Enum
-    "",                          -- Keyword
-    "﬌",                          -- Snippet
-    "",                          -- Color
-    "",                          -- File
-    "",                          -- Reference
-    "",                          -- Folder
-    "",                          -- EnumMember
-    "",                          -- Constant
-    "",                          -- Struct
-    "",                          -- Event
-    "ﬦ",                          -- Operator
-    ""                           -- TypeParameter
+    "",                           -- Method
+    "",                           -- Function
+    "",                           -- Constructor
+    "",                           -- Field
+    "",                           -- Variable
+    "",                           -- Class
+    "ﰮ",                           -- Interface
+    "",                           -- Module
+    "",                           -- Property
+    "",                           -- Unit
+    "",                           -- Value
+    "",                           -- Enum
+    "",                           -- Keyword
+    "﬌",                           -- Snippet
+    "",                           -- Color
+    "",                           -- File
+    "",                           -- Reference
+    "",                           -- Folder
+    "",                           -- EnumMember
+    "",                           -- Constant
+    "",                           -- Struct
+    "",                           -- Event
+    "ﬦ",                           -- Operator
+    ""                            -- TypeParameter
 }
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
